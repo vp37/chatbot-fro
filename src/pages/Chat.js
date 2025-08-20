5da3056cbc03035b27df9426d00f2763f767895e
vp37 <vigneshprabutbvsh@gmail.com>
2025-08-20 17:55:14 +0530

    completed

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -63,7 +63,7 @@ function App() {
   const chatEndRef = useRef(null);
 
   // Backend URL - set only production URL here
-  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://chatbot-bac-ulai.onrender.com";
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://chatbot-bac-1.onrender.com";
 
   const sendMessage = async () => {
     if (!input) return;
